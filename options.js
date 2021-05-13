e62e49d7238d993510edfeca41c61b4df3b0f553
Gareth <freedom.2.the.leetle.people@gmail.com>
2021-05-14 00:35:10 +1000

    more fully fleshed out options
    using browser_style and chrome_style
    fixed up options.js so that it saves and losds options.

--- options.js
+++ options.js
@@ -1,1 +1,26 @@
 // options.js
+var opts = [
+	"enablenine",
+	"fullblock"
+	];
+
+function saveOptions(e) {
+	opts.map((opt)=>{
+		var sval = {};
+		sval[opt] = document.querySelector(`#${opt}`).checked;
+		browser.storage.sync.set(sval);		
+	});
+	alert("Saved!");
+  e.preventDefault();
+}
+
+function restoreOptions() {
+  browser.storage.sync.get().then((res) => {
+  	opts.map((opt)=>{
+			document.querySelector(`#${opt}`).checked = res[opt];
+		});
+  });
+}
+
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.querySelector("form").addEventListener("submit", saveOptions);
